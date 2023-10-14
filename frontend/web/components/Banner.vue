23b7e10e132e9574c8492acb302382561f484458
saya <c3313433633@gmail.com>
2023-10-14 23:00:28 +0800

    nuxt

---
+++ frontend/web/components/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false" @select="handleSelect">
+        <el-menu-item index="0">
+            <img style="width: 100px" src="/element-plus-logo.svg" alt="Element logo" />
+        </el-menu-item>
+        <div class="flex-grow" />
+        <el-menu-item index="1">
+            <ElButton type="primary">
+                下载
+            </ElButton>
+        </el-menu-item>
+        <el-menu-item index="2">
+            <ElButton type="primary">
+                上传
+            </ElButton>
+        </el-menu-item>
+        <el-sub-menu index="3">
+            <template #title>用户</template>
+            <el-menu-item index="3-1">设置</el-menu-item>
+            <el-menu-item index="3-2">退出</el-menu-item>
+        </el-sub-menu>
+    </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
+</script>
+
+<style>
+.flex-grow {
+    flex-grow: 1;
+}
+</style>
--- frontend/web/pages/index.vue
+++ frontend/web/pages/index.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="page-index">
-    <Logos />
-    <Examples />
-  </div>
+    <el-radio-group v-model="labelPosition" label="label position">
+        <el-radio-button label="left">Left</el-radio-button>
+        <el-radio-button label="right">Right</el-radio-button>
+        <el-radio-button label="top">Top</el-radio-button>
+    </el-radio-group>
+    <div style="margin: 20px" />
+    <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign" style="max-width: 460px">
+        <el-form-item label="Name">
+            <el-input v-model="formLabelAlign.name" />
+        </el-form-item>
+        <el-form-item label="Activity zone">
+            <el-input v-model="formLabelAlign.region" />
+        </el-form-item>
+        <el-form-item label="Activity form">
+            <el-input v-model="formLabelAlign.type" />
+        </el-form-item>
+    </el-form>
 </template>
+
+<script lang="ts" setup>
+import type { FormProps } from 'element-plus';
+import { reactive, ref } from 'vue';
+
+const labelPosition = ref<FormProps['labelPosition']>('right')
+
+const formLabelAlign = reactive({
+    name: '',
+    region: '',
+    type: '',
+})
+</script>
 
 <style>
 .page-index {
-  padding-top: 60px;
-  text-align: center;
+    padding-top: 60px;
+    text-align: center;
 }
 </style>
